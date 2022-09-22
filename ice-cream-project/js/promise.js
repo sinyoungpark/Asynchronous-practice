7346d5925d858a251762f32c8f46c32199a15a00
sinyoung98 <joyhope3@naver.com>
2022-09-23 04:20:11 +0900

    Add promise chaining code
    .then

--- ice-cream-project/js/promise.js
+++ ice-cream-project/js/promise.js
@@ -21,5 +21,36 @@ let order = (time, work) => {
         }
     });
 }
+/*then 
+1. then 과 then 사이 ; 사용불가 
+2. then 과 then 사이에는 스페이스 무수히 가능.
+*/
+order(2000, () => console.log(`${stocks.Fruits[0]} was selected`))
 
-order(2000, () => console.log(`${stocks.Fruits[0]} was selected`));
+.then(() => {
+    return order(0000, () => console.log("production has started"));
+})
+
+.then(() => {
+    return order(2000, () => console.log("the fruit was chopped"));
+})
+
+.then(() => {
+    return order(1000, () =>console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added`));
+})
+
+.then(() => {
+    return order(1000, () =>console.log("the machine was started"));
+})
+
+.then(() => {
+    return order(2000, () =>console.log(`ice cream was placed on ${stocks.holder[0]}`));
+})
+
+.then(() => {
+    return order(3000, () =>console.log(`${stocks.toppings[0]} was added as toppings`));
+})
+
+.then(() => {
+    return order(2000, () => console.log("serve ice cream"));
+})
